0f4abc2fc1df6995727aa860754d38de311e7605
CodenotchSchool <codenotch@codenotch.com>
2022-08-29 10:17:09 +0200

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Home2Component } from './pages/home2/home2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home2Component
   ],
   imports: [
     BrowserModule,
